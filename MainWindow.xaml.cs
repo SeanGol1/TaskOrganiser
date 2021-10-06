8f78e300ee416ea4442eb793e2d4756611ab39cf
seangol1 <Seangol1@hotmail.com>
2021-10-06 22:55:39 +0100

    Fix List View

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -88,7 +88,22 @@ namespace TaskOrgraniser
         {
             try
             {
+                int counter = 0;
+                string line;
 
+                // Read the file and display it line by line.  
+                System.IO.StreamReader file =
+                    new System.IO.StreamReader(@"C:/Users/seang/Desktop/IN DEV/TaskOrgraniser/SaveFile.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    System.Console.WriteLine(line);
+                    counter++;
+                }
+
+                file.Close();
+                System.Console.WriteLine("There were {0} lines.", counter);
+                // Suspend the screen.  
+                System.Console.ReadLine();
 
             }
             catch (Exception ex)
